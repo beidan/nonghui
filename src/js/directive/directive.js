dfdf41657cf5390b4c3ff69fb7b64310a1a64cd8
peidan <m18814127307@163>
2016-08-14 18:08:54 +0800

    add

--- src/js/directive/directive.js
+++ src/js/directive/directive.js
@@ -14,7 +14,7 @@ app.directive("footNav", function () {
     return {
         restrict: 'E',
         replace: true,
-        template: '<footer><a href="#/"><p class="fa fa-home icon" aria-hidden="true"></p></i><p>首页</p></a><a href="#/asort"><p class="fa fa-bars icon" aria-hidden="true"></p><p>分类</p></a>'+
+        template: '<footer><a href="#/"><p class="fa fa-home icon" aria-hidden="true"></p></i><p>首页</p></a><a href="#/asort?cate=fruits"><p class="fa fa-bars icon" aria-hidden="true"></p><p>分类</p></a>'+
         '<a href="#/cart"><p class="fa fa-shopping-cart icon" aria-hidden="true"></p><p>购物车</p></a><a href="#/user"><p class="fa fa-user icon" aria-hidden="true"></p><p>我的</p></a></footer>'
     };
 });
